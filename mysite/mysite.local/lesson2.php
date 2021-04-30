af94b8929df3527497d7c8c82da088d0a6cf9c44
idmitry269 <70436265+idmitry269@users.noreply.github.com>
2021-04-30 14:34:18 +0300

    Обновление Урок2
    Замена покраски цифр циклом на str_replace

--- mysite/mysite.local/lesson2.php
+++ mysite/mysite.local/lesson2.php
@@ -10,40 +10,22 @@
     <table>
         <?php
         $multiplier = 1;
-        $red_1 = "<span class='r1'>1</span>";
-        $green_2 = "<span class='g2'>2</span>";
-        $yellow_3 = "<span class='y3'>3</span>";
-        $blue_4 = "<span class='b4'>4</span>";
+        $number = ['1', '2', '3', '4'];
+        $num_col = [
+            "<span class='r1'>1</span>",
+            "<span class='g2'>2</span>",
+            "<span class='y3'>3</span>",
+            "<span class='b4'>4</span>"
+        ];
         for ($t_l_score = 1; $t_l_score <= 2; $t_l_score++) {
             echo "<tr>";
             for ($t_c_score = 1; $t_c_score <= 5; $t_c_score++) {
                 echo "<td class=table>";
                 for ($multiplicand = 1; $multiplicand <= 10; $multiplicand++) {
-                    $res_arr = [];
                     $result = $multiplier * $multiplicand;
                     $res_str = "$multiplier x $multiplicand = $result<br>";
-                    $res_str_len = strlen($res_str);
                     echo "<tr1>";
-                    for ($res_key = 0; $res_key < $res_str_len; $res_key++) {
-                        switch ($res_str[$res_key]) {
-                            case 1:
-                                $res_arr[$res_key] = $red_1;
-                                break;
-                            case 2:
-                                $res_arr[$res_key] = $green_2;
-                                break;
-                            case 3:
-                                $res_arr[$res_key] = $yellow_3;
-                                break;
-                            case 4:
-                                $res_arr[$res_key] = $blue_4;
-                                break;
-                            default:
-                                $res_arr[$res_key] = $res_str[$res_key];
-                        }
-                    }
-                    $res_screen = implode($res_arr);
-                    echo $res_screen;
+                    echo str_replace($number, $num_col, $res_str);
                 }
                 $multiplier++;
             }
